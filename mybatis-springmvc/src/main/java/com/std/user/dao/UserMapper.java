ddffb3c36d77a2dc2d757a5bcf1545ffcd6723cc
liushengqi <liushengqi@lvye.com>
2015-06-22 15:52:59 +0800

    commit mybatis springmvc webapp

---
+++ mybatis-springmvc/src/main/java/com/std/user/dao/UserMapper.java
@@ -0,0 +1,38 @@
+package com.std.user.dao;
+
+import com.std.user.domain.User;
+
+/**
+ * Created by sence on 2015/6/21.
+ */
+public interface UserMapper {
+
+    /**
+     * 保存用户
+     * @param user
+     * @return
+     */
+    public Integer saveUser(User user);
+
+    /**
+     * 更新用户
+     * @param user
+     * @return
+     */
+ //   public int updateUser(User user);
+
+    /**
+     * 查找用户
+     * @param userId
+     * @return
+     */
+    public User selectUser(Integer userId);
+
+    /**
+     * 查找users
+     * @param user
+     * @return
+     */
+  //  public List<User> selectUsers(User user);
+
+}
