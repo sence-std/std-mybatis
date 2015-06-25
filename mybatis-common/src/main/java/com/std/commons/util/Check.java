bc6ac93228e83299eceb020b28880a652ad5ccce
sence <lovesenceqi@gmail.com>
2015-06-25 09:17:54 +0800

    add pom

--- mybatis-common/src/main/java/com/std/commons/util/Check.java
+++ mybatis-common/src/main/java/com/std/commons/util/Check.java
@@ -1,11 +1,52 @@
 package com.std.commons.util;
 
+import java.util.Collection;
+
 /**
  * Created by sence on 2015/6/22.
  */
 public class Check {
 
+    /**
+     * 判断对象是否为空
+     * @param obj
+     * @return boolean
+     */
     public static boolean isNull(Object obj){
         return obj == null;
     }
+
+    /**
+     * 判断是否是空字符串
+     * @param str
+     * @return
+     */
+    public static boolean isBlankStr(String str){
+        if(isNull(str)){
+            return true;
+        }
+        if("".equals(str)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断是否是空集合
+     * @param collect
+     * @return
+     */
+    public static boolean isEmptyCollection(Collection collect){
+        if(isNull(collect)){
+            return true;
+        }
+        if(collect.size()==0){
+            return true;
+        }
+        if(collect.contains(null)){
+            return true;
+        }
+        return false;
+    }
+
 }
--- mybatis-common/src/test/java/com/std/commons/util/T_Check.java
+++ mybatis-common/src/test/java/com/std/commons/util/T_Check.java
@@ -1,7 +1,24 @@
 package com.std.commons.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by sence on 6/23/2015.
  */
 public class T_Check {
+
+    @Test
+    public void testEmptyCollection(){
+        List<String> strs = new ArrayList<>();
+        strs.add(null);
+        strs.add("a");
+        strs.add("b");
+        Assert.assertEquals(3,strs.size());
+        Assert.assertEquals(true,Check.isEmptyCollection(strs));
+    }
+
 }
